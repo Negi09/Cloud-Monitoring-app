6353ebb54884e198d68cb345ac8e290713472912
Negi09 <bhaveshnegi07@gmail.com>
2025-04-30 07:41:57 +0000

    docker file committed

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app = Flask(__name__) # setting up the app name
 def index():
     cpu_usage = psutil.cpu_percent(interval=1)  # stores the CPU usage
     memory_usage = psutil.virtual_memory().percent  # stores the memory usage percentage
-
+    info=None
     if cpu_usage > 80 or memory_usage > 80:
-        print("High CPU or memory usage detected!")
+        msg ="High CPU or memory usage detected!"
 
-    return render_template("index.html", cpu_metric=cpu_usage, memory_metric=memory_usage)  # renders the index.html file
+    return render_template("index.html", cpu_metric=cpu_usage, mem_metric=memory_usage,message =info  )# renders the index.html file
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0",port=5000)
 
 
 #returns a log file everytime this script is made to run 
---
+++ dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.12.3-slim
+
+WORKDIR /app
+
+ # copies the requirements file to the  docker container.
+COPY requirement.txt .
+
+RUN pip3 install --no-cache-dir -r requirement.txt
+
+# copies the rest of the application code to the docker container.!
+COPY . . 
+
+ #available to all IP addresses.!(host is the variable name..)
+ENV host="0.0.0.0"
+
+# listing port .!!
+EXPOSE 5000
+
+CMD ["flask", "run"]
